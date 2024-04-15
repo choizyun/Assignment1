1d542e005ba56f708541f3ec76236140eea44add
choizyun <jihyun6455@gmail.com>
2024-04-15 14:58:49 +0900

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by 최지현 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
